16d86b61a870f2d1746761141e40ee80f8a67194
Marco <marco1002@live.com>
2021-03-04 15:51:12 -0300

    suceso

--- TestarConexao/Form1.cs
+++ TestarConexao/Form1.cs
@@ -18,15 +18,7 @@ namespace TestarConexao
             {
                 FbConnection conexao = new FbConnection(@$"User={txtUsuario.Text}; Password={txtSenha.Text}; Database={txtDatabase.Text}; DataSource={txtHost.Text}; Port=3050; Dialect=3; Charset=NONE; Role=; Connection lifetime=15; Pooling=true; MinPoolSize=0; MaxPoolSize=50; Packet Size=8192; ServerType=0");
                 var command = conexao.CreateCommand();
-                //command.CommandText = @"SHOW DATABASES";
-
                 await conexao.OpenAsync();
-                //var result = command.ExecuteReader();
-                //while (result.Read())
-                //{
-                //    int minutos = result.GetInt32(4);
-                //    l.Add(new EventoDTO(result.GetDecimal(0), result.GetDateTime(1), result.GetInt32(2), result.GetString(3), minutos));
-                //}
                 Interaction.MsgBox("Conexão testada com sucesso", MsgBoxStyle.Information);
             }
             catch (Exception erro)
